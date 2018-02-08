99a07e9a967932184b4ce068a4e5ec99be9aa9cc
bahalool <bahalool@gmail.com>
2018-02-08 15:01:49 +0200

    fixed index

--- index.js
+++ index.js
@@ -1,21 +1,2 @@
 
-const S3Client = require('@hkube/s3-client');
-
-class S3Adapter {
-    constructor() {
-        this._client = null;
-    }
-
-    async init(connection) {
-        if (this._client == null) {
-            return S3Client.init(connection);
-        }
-        return this._client;
-    }
-
-    async put(options) {
-        return S3Client.put({ Bucket: options.pipelineId, Key: options.taskId, Body: options.data });
-    }
-}
-
-module.exports = new S3Adapter();
+module.exports = require('./lib/s3-adapter');
