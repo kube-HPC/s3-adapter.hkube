2b880d7be7ba422e2376f632352a1eeae099002c
NassiHarel <nassi9@gmail.com>
2020-04-05 12:35:04 +0300

    fix tests

--- tests/test.js
+++ tests/test.js
@@ -1,8 +1,8 @@
 const { expect } = require('chai');
 const path = require('path');
-const { EncodingTypes } = require('@hkube/encoding');
-const S3Adapter = require('../lib/s3-adapter');
 const moment = require('moment');
+const { EncodingTypes, Encoding } = require('@hkube/encoding');
+const S3Adapter = require('../lib/s3-adapter');
 const BUCKETS_NAMES = {
     HKUBE: 'hkube',
     HKUBE_RESULTS: 'hkube-results',
@@ -12,67 +12,52 @@ const BUCKETS_NAMES = {
     HKUBE_INDEX: 'hkube-index'
 };
 const DateFormat = 'YYYY-MM-DD';
-const adapter = new S3Adapter();
+let adapter = new S3Adapter();
 
-const { Encoding } = require('@hkube/encoding');
-const encoding = new Encoding({ type: 'json' });
+const options = {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID || 'AKIAIOSFODNN7EXAMPLE',
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY',
+    endpoint: process.env.AWS_ENDPOINT || 'http://127.0.0.1:9000'
+};
 
-EncodingTypes.forEach((o) => {
-    describe(`s3-adapter ${o}`, () => {
-        before(async () => {
-            const options = {
-                accessKeyId: process.env.AWS_ACCESS_KEY_ID || 'AKIAIOSFODNN7EXAMPLE',
-                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY',
-                endpoint: process.env.AWS_ENDPOINT || 'http://127.0.0.1:9000',
-                ...o
-            };
-            adapter._wasInit = false;
-            await adapter.init(options, BUCKETS_NAMES, true);
+describe(`s3-adapter`, () => {
+    before(async () => {
+        await adapter.init(options, BUCKETS_NAMES, true);
+    });
 
-            const wrapperGet = (fn) => {
-                const wrapper = async (args) => {
-                    const result = await fn(args);
-                    return encoding.decode(result);
+    EncodingTypes.forEach((o) => {
+        describe(`s3-adapter ${o}`, () => {
+            before(async() => {
+                adapter = new S3Adapter();
+                await adapter.init(options, BUCKETS_NAMES, false);
+
+                const encoding = new Encoding({ type: o });
+
+                const wrapperGet = (fn) => {
+                    const wrapper = async (args) => {
+                        const result = await fn(args);
+                        return encoding.decode(result);
+                    };
+                    return wrapper;
                 };
-                return wrapper;
-            };
-    
-            const wrapperPut = (fn) => {
-                const wrapper = (args) => {
-                    const data = (!args.ignoreEncode && encoding.encode(args.data)) || args.data;
-                    return fn({ ...args, data });
+
+                const wrapperPut = (fn) => {
+                    const wrapper = (args) => {
+                        const data = (!args.ignoreEncode && encoding.encode(args.data)) || args.data;
+                        return fn({ ...args, data });
+                    };
+                    return wrapper;
                 };
-                return wrapper;
-            };
-    
-            adapter.put = wrapperPut(adapter.put.bind(adapter));
-            adapter.get = wrapperGet(adapter.get.bind(adapter));
-        });
-        describe(`put ${o}`, () => {
-            it(`put and get same value ${o}`, async () => {
-                await adapter.put({ path: path.join(BUCKETS_NAMES.HKUBE, o, moment('2015-01-27').format(DateFormat), 'test1', 'test1.json'), data: { data: 'sss' } });
-                await adapter.put({ path: path.join(BUCKETS_NAMES.HKUBE, o, moment('2015-01-26').format(DateFormat), 'test1', 'test1.json'), data: { data: 'sss' } });
-                await adapter.put({ path: path.join(BUCKETS_NAMES.HKUBE, o, moment('2015-01-25').format(DateFormat), 'test1', 'test1.json'), data: { data: 'sss' } });
-                await adapter.put({ path: path.join(BUCKETS_NAMES.HKUBE, o, moment('2015-01-24').format(DateFormat), 'test1', 'test1.json'), data: { data: 'sss' } });
-                await adapter.put({ path: path.join(BUCKETS_NAMES.HKUBE, o, moment('2015-01-23').format(DateFormat), 'test1', 'test1.json'), data: { data: 'sss' } });
-                await adapter.put({ path: path.join(BUCKETS_NAMES.HKUBE, o, moment('2015-01-22').format(DateFormat), 'test1', 'test1.json'), data: { data: 'sss' } });
 
-                await adapter.put({ path: path.join(BUCKETS_NAMES.HKUBE, o, moment('2018-06-21').format(DateFormat), 'test1', 'test1.json'), data: { data: 'sss' } });
-                await adapter.put({ path: path.join(BUCKETS_NAMES.HKUBE, o, moment('2018-06-22').format(DateFormat), 'test1', 'test1.json'), data: { data: 'sss' } });
-                await adapter.put({ path: path.join(BUCKETS_NAMES.HKUBE, o, moment('2018-06-23').format(DateFormat), 'test1', 'test1.json'), data: { data: 'sss' } });
-                await adapter.put({ path: path.join(BUCKETS_NAMES.HKUBE, o, moment('2018-06-24').format(DateFormat), 'test1', 'test1.json'), data: { data: 'sss' } });
-                await adapter.put({ path: path.join(BUCKETS_NAMES.HKUBE, o, moment('2018-06-25').format(DateFormat), 'test1', 'test1.json'), data: { data: 'sss' } });
-
-                const link = await adapter.put({ path: path.join(BUCKETS_NAMES.HKUBE, o, moment().format(DateFormat), Date.now().toString(), 'task-1'), data: 'test' });
-                const res = await adapter.get(link);
-                expect(res).to.equal('test');
+                adapter.put = wrapperPut(adapter.put.bind(adapter));
+                adapter.get = wrapperGet(adapter.get.bind(adapter));
             });
-            it(`put result ${o}`, async () => {
+            it(`put result`, async () => {
                 const link = await adapter.put({ path: path.join(BUCKETS_NAMES.HKUBE_RESULTS, o, moment().format(DateFormat), 'job-id', 'result.json'), data: 'test' });
                 const res = await adapter.get(link);
                 expect(res).to.equal('test');
             });
-            it(`get all tasks of specific jobid ${o}`, async () => {
+            it(`get all tasks of specific jobid`, async () => {
                 const jobId = Date.now();
                 const results = await Promise.all([
                     adapter.put({ path: path.join(BUCKETS_NAMES.HKUBE, o, `${moment().format(DateFormat)}/${jobId}/0`), data: 'test0' }),
@@ -91,7 +76,7 @@ EncodingTypes.forEach((o) => {
                     expect(r).to.equal('test' + i);
                 }
             });
-            it(`get more than 3000 items ${o}`, async () => {
+            it(`get more than 3000 items`, async () => {
                 const promiseArray = [];
                 for (let i = 0; i < 3500; i += 1) {
                     promiseArray.push(adapter.put({ path: path.join(BUCKETS_NAMES.HKUBE, o, o, moment().format(DateFormat), 'more-than-3000-keys', 'task' + i), data: `test${i}` }));
@@ -100,7 +85,7 @@ EncodingTypes.forEach((o) => {
                 const res = await adapter.list({ path: path.join(BUCKETS_NAMES.HKUBE, o, o, moment().format(DateFormat), 'more-than-3000-keys') });
                 expect(res.length).to.equal(3500);
             }).timeout(80000);
-            it(`delete more than 3000 items ${o}`, async () => {
+            it(`delete more than 3000 items`, async () => {
                 {
                     const promiseArray = [];
                     for (let i = 0; i < 3500; i += 1) {
@@ -115,7 +100,7 @@ EncodingTypes.forEach((o) => {
                     expect(res2.length).to.equal(0);
                 }
             }).timeout(80000);
-            it(`delete by date ${o}`, async () => {
+            it(`delete by date`, async () => {
                 await adapter.put({ path: path.join(BUCKETS_NAMES.HKUBE, o, moment('2015-01-14').format(DateFormat), 'test1', 'test1.json'), data: { data: 'sss' } });
                 await adapter.put({ path: path.join(BUCKETS_NAMES.HKUBE, o, moment('2015-01-14').format(DateFormat), 'test2', 'test2.json'), data: { data: 'sss' } });
                 await adapter.put({ path: path.join(BUCKETS_NAMES.HKUBE, o, moment('2015-01-14').format(DateFormat), 'test3', 'test3.json'), data: { data: 'sss' } });
@@ -154,7 +139,7 @@ EncodingTypes.forEach((o) => {
                 const res3 = await adapter.list({ path: path.join(BUCKETS_NAMES.HKUBE, o, '2015-01-14/test3') });
                 expect(res3.length).to.equal(0);
             }).timeout(80000);
-            it(`delete by date more than 3000 items ${o}`, async () => {
+            it(`delete by date more than 3000 items`, async () => {
                 const promiseArray = [];
                 for (let i = 0; i < 3500; i += 1) {
                     promiseArray.push(adapter.put({ path: path.join(BUCKETS_NAMES.HKUBE, o, moment('2014-11-28').format(DateFormat), 'test3', `test${i}.json`), data: { data: 'sss' } }));
@@ -164,7 +149,7 @@ EncodingTypes.forEach((o) => {
                 const res2 = await adapter.list({ path: path.join(BUCKETS_NAMES.HKUBE, o, '2014-11-28') });
                 expect(res2.length).to.equal(0);
             }).timeout(80000);
-            it(`list objects without prefix ${o}`, async () => {
+            it(`list objects without prefix`, async () => {
                 const jobId = Date.now().toString();
                 await Promise.all([
                     adapter.put({ path: path.join(BUCKETS_NAMES.HKUBE, o, moment().format(DateFormat), jobId, '0'), data: 'test0' }),
@@ -176,7 +161,7 @@ EncodingTypes.forEach((o) => {
                 const res2 = await adapter.list({ path: path.join(BUCKETS_NAMES.HKUBE_RESULTS, o, '/') });
                 expect(res2.length > 0).to.be.true;
             }).timeout(80000);
-            it(`list objects with delimiter ${o}`, async () => {
+            it(`list objects with delimiter`, async () => {
                 const jobId = Date.now().toString();
                 await adapter.put({ path: path.join(BUCKETS_NAMES.HKUBE_INDEX, '2019-01-01', jobId, '0'), data: { data: 'sss1' } });
                 await adapter.put({ path: path.join(BUCKETS_NAMES.HKUBE_INDEX, '2019-01-02', jobId, '1'), data: { data: 'sss2' } });
@@ -188,5 +173,5 @@ EncodingTypes.forEach((o) => {
                 expect(rd.includes('2019-01-03')).to.be.true;
             }).timeout(80000);
         });
-    });
+    })
 })
